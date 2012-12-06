09ee9bc3df7f0431e91496e2051f15e711baa290
Mateusz Parzonka <parzonka@gmail.com>
2012-12-06 02:26:59 +0100

    Add more tests

--- src/test/java/jabular/TabularTest.java
+++ src/test/java/jabular/TabularTest.java
@@ -16,11 +16,41 @@ import org.junit.Test;
 
 public class TabularTest {
 
+    @Test(expected=IllegalArgumentException.class)
+    public void setValue_unknownLabel() throws Exception {
+	Tabular tabular = Tabular.getBuilder().setColumns(1).setRows(1).build();
+	tabular.setValue("42", "0", "Foo");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void getValue_unknownLabel() throws Exception {
+	Tabular tabular = Tabular.getBuilder().setColumns(1).setRows(1).build();
+	tabular.setValue("0", "0", "Foo");
+	tabular.getValue("42", "0");
+    }
+
     @Test
-    public void getBuilder() throws Exception {
+    public void getValue_tabularWithNumberLabels() throws Exception {
 	Tabular tabular = Tabular.getBuilder().setColumns(1).setRows(1).build();
 	tabular.setValue("0", "0", "Foo");
 	assertEquals("Foo", tabular.getValue("0", "0"));
+    }
+
+    @Test
+    public void getValue_tabularWithStringLabels() throws Exception {
+	Tabular tabular = Tabular.getBuilder().setColumns("a", "b", "c").setRows(2).build();
+	tabular.setValue("0", "a", "0-a");
+	tabular.setValue("0", "b", "0-b");
+	tabular.setValue("0", "c", "0-c");
+	tabular.setValue("1", "a", "1-a");
+	tabular.setValue("1", "b", "1-b");
+	tabular.setValue("1", "c", "1-c");
+	assertEquals("0-a", tabular.getValue("0", "a"));
+	assertEquals("0-b", tabular.getValue("0", "b"));
+	assertEquals("0-c", tabular.getValue("0", "c"));
+	assertEquals("1-a", tabular.getValue("1", "a"));
+	assertEquals("1-b", tabular.getValue("1", "b"));
+	assertEquals("1-c", tabular.getValue("1", "c"));
     }
 
 }
